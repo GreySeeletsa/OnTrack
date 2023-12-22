80313ef4742224a78e0431807d221d9792e514d4
GreySeeletsa <91868401+GreySeeletsa@users.noreply.github.com>
2023-12-22 11:15:00 +0200

    Update Avatar.js adding comments

--- Avatar.js
+++ Avatar.js
@@ -1,8 +1,11 @@
 import blankAvatar from '../images/blank-profile.png'
 
+// Define a functional component to display the avatar
 const AvatarDisplay = ({ ticket }) => {
   return (
+    // Create a container for the avatar
 <div className="avatar-container">
+    // Create a container for the image
 <div className="img-container">
 <img src={ticket.avatar ? ticket.avatar : blankAvatar} alt={'photo of ' + ticket.owner} />
 </div>
@@ -10,4 +13,5 @@ const AvatarDisplay = ({ ticket }) => {
   )
 }
 
+// Export the AvatarDisplay component
 export default AvatarDisplay
